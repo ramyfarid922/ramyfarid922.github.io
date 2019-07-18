e59405eedc1d7bab6dac2006d56db2d37d181a7a
ramy <ramyfarid922@gmail.com>
2019-07-18 18:49:19 +0200

    Fixes blue frame

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,32 @@
 import React, { Component } from "react";
 import { Route, Switch } from "react-router-dom";
-import NavigationBar from "./NavigationBar";
 import NavigationBarSB from "./NavigationBarSB";
-
 import FlashMessagesList from "./flash/FlashMessagesList";
-import Greetings from "./Greetings";
 import SignupPage from "./signup/SignupPage";
 import LoginPage from "./login/LoginPage";
 import HomePage from "./personal/HomePage";
 import PortfolioPage from "./personal/PortfolioPage";
 import BlogPage from "./personal/BlogPage";
+import "./App.css";
 
 export default class App extends Component {
   render() {
     return (
-      <div>
+      <div className="app-container">
         {/* <NavigationBar /> */}
         <NavigationBarSB />
         <FlashMessagesList />
 
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route path="/signup" component={SignupPage} />
-          <Route path="/home" component={HomePage} />
-          <Route path="/portfolio" component={PortfolioPage} />
-          <Route path="/blog" component={BlogPage} />
-          <Route path="/login" component={LoginPage} />
-        </Switch>
+        <div className="main-content">
+          <Switch>
+            <Route exact path="/" component={HomePage} />
+            <Route path="/signup" component={SignupPage} />
+            <Route path="/home" component={HomePage} />
+            <Route path="/portfolio" component={PortfolioPage} />
+            <Route path="/blog" component={BlogPage} />
+            <Route path="/login" component={LoginPage} />
+          </Switch>
+        </div>
       </div>
     );
   }
