0948e64b1414c4fbbe82635c9d9d85f0656fcade
ramy <ramyfarid922@gmail.com>
2019-07-18 14:05:41 +0200

    Updates

---
+++ precache-manifest.42ccf6350608d5165e3a728558a63a2a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54478ce2be2f2928cfd7c279f039036f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e1403484a9c3b8a653f",
+    "url": "/static/css/main.316e13f2.chunk.css"
+  },
+  {
+    "revision": "09996741a62c9e6cad3a",
+    "url": "/static/js/2.03510ab2.chunk.js"
+  },
+  {
+    "revision": "8e1403484a9c3b8a653f",
+    "url": "/static/js/main.5c93ea82.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
