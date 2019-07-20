5ef67770fc6afa9d2a64a8e8ce53e2132567824d
ramy <ramyfarid922@gmail.com>
2019-07-20 15:37:34 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.364929c85905f1810f383a858de1c87a.js"
+  "/precache-manifest.30822cc4049851b2c302153ddb775407.js"
 );
 
 self.addEventListener('message', (event) => {
